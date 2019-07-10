b72f68602d3dfb7ebb87b57da10f81e74317b13e
Max <ferromassimo1989@gmail.com>
2019-07-10 13:38:09 +0200

    cleaning code

--- server.js
+++ server.js
@@ -16,25 +16,33 @@ const db=knex({
 	}
 });
 
-
+const check=(data)=>{
+	if (data.split('').filter(x => x === '{').length >= 1) {
+		return true
+	}else{
+		return false
+	}
+}
 
 const app=express();
 app.use(bodyParser.json());
 app.use(cors());
 
-
+//selecting and sending all the sites from the db to the FE
 app.get('/',(req,res)=>{
 	db.select('*').from('sites').orderBy('m_sites_id','desc').then(sites=>{
 		res.json(sites)
 	})
 })
 
+//selecting the favorite sites from the db
 app.get('/favorite',(req,res)=>{
 	db.select('*').from('sites').whereIn('m_sites_id',[1,2,3,5,11]).orderBy('m_sites_id','desc').then(sites=>{
 		res.json(sites)
 	})
 })
 
+//getting more info about a site(id) and sending it to the FE 
 app.get('/moreinfo/:id',(req,res)=>{
 	const {id}=req.params;
 	db.select('*').from('sites').where('m_sites_id','=',id).then(sites=>{
@@ -42,14 +50,13 @@ app.get('/moreinfo/:id',(req,res)=>{
 	})
 })
 
+//sending mail thru nodemailer
 app.post('/sendmail',(req,res)=>{
 	const {name,email,message}=req.body;
-	if(!name || 
-		name.split('').filter(x => x === '{').length === 1 ||
-		!email||email.split('').filter(x => x === '{').length === 1 ||
-		!message||message.split('').filter(x => x === '{').length === 1){
+	if(!name ||	check(name) ||!email||check(email) ||!message||check(message)){
 		return res.status(400).json('Incorrect form.')
 	}else {
+		//setting the transporter
 		let transporter = nodemailer.createTransport({
 					service: 'yahoo',		        
 					auth: {
@@ -57,12 +64,14 @@ app.post('/sendmail',(req,res)=>{
 		            pass: `${process.env.email_pass}` // generated ethereal password
 		        }
 		    });	
+		//setting the mail form
 				let mailOptions = {
 		        from: 'TestNodemailerYelcamp@yahoo.com', // sender address
 		        to: 'ferromassimo1989@gmail.com', // list of receivers
 		        subject: 'Work', // Subject line
 		        text: `Email:${email} Name:${name} Message:${message}`
 		      };
+		      //sending the mail
 		      transporter.sendMail(mailOptions, (error, info) => {
 		      	if (error) {
 		      		console.log(error)
@@ -76,11 +85,10 @@ app.post('/sendmail',(req,res)=>{
 	
 })
 
+//inserting new comment in the db
 app.post('/newComment',(req,res)=>{
 	const {comment_text,username,sites_id,comment_added}=req.body;
-	if(!user_id || 
-		user_id.split('').filter(x => x === '{').length === 1 ||
-		!comment_text||comment_text.split('').filter(x => x === '{').length === 1 ){
+	if(!user_id ||check(user_id)||!comment_text||check(comment_text) ){
 		return res.status(400).json('Incorrect form.')
 	}else{
 		db('comments')
@@ -99,6 +107,7 @@ app.post('/newComment',(req,res)=>{
 	
 })
 
+//getting all the comments from the db
 app.get('/allComment/:site_id',(req,res)=>{
 	const {site_id}=req.params;
 	db.from('comments').innerJoin('sites', 'sites.m_sites_id', 'comments.site_id')	
